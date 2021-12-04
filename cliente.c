9c0b7673a611b14c57b3c18194ff3c38e2c51e4b
Ubuntu <arleth@web-server.fczgpsggm0fenmn0wergw4lrrg.bx.internal.cloudapp.net>
2021-12-04 19:53:17 +0000

    Servidor y Cliente

---
+++ cliente.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* sockets */
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#define IP_SERVIDOR "127.0.0.1"
+
+int main() {
+	int socket_cliente;
+	int puerto;
+	char ip_servidor[10];
+	int tam_struct_cliente;
+	int estadoConexion;
+
+	char cadenaCliente[1024];
+    char cadenaServidor[1024];
+
+	socket_cliente = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in servidor_direccion;
+    struct sockaddr_in cliente_direccion;
+
+    printf("Ingresa el puerto del servidor:\n");
+    scanf("%d", &puerto);
+
+	cliente_direccion.sin_family = AF_INET;
+	cliente_direccion.sin_addr.s_addr = inet_addr(IP_SERVIDOR);
+	cliente_direccion.sin_port = puerto;
+	tam_struct_cliente = sizeof(cliente_direccion);
+
+    estadoConexion = connect(socket_cliente, (struct sockaddr *)&cliente_direccion, tam_struct_cliente );
+    if(estadoConexion == -1 ){
+         printf("Problemas al conectar con servidor... \n");
+         exit(-1);
+         close(socket_cliente);
+    }
+    printf("\t--SESION INICIADA CON EL SERVIDOR--\n");
+
+	while(strcmp(cadenaCliente, "BREAK") != 0) {
+
+        recv(socket_cliente, cadenaServidor, 1024, 0);
+        if(strcmp(cadenaServidor, "BREAK") == 0) {
+        	break;
+        } else {
+        	printf("%s", cadenaServidor); // la palabra es
+        }
+
+		recv(socket_cliente, cadenaServidor, 1024, 0);
+		printf("%s", cadenaServidor); // _ _ _ _ _ _ _
+
+		recv(socket_cliente, cadenaServidor, 1024, 0);
+		printf("%s", cadenaServidor); // ingrese una
+
+		printf("Letra: ");
+		scanf("%*c%[^\n]", cadenaCliente);
+		send(socket_cliente, cadenaCliente, 1024, 0);
+
+		recv(socket_cliente, cadenaServidor, 1024, 0);
+		//printf("OPOR: %s\n", cadenaServidor);
+        if(strcmp(cadenaServidor, "NO") == 0) {
+        	recv(socket_cliente, cadenaServidor, 1024, 0);
+			printf("%s", cadenaServidor); // ingrese una
+        }
+  	}
+  	recv(socket_cliente, cadenaServidor, 1024, 0);
+  	printf("%s", cadenaServidor);
+
+    close(socket_cliente);
+}
---
+++ servidor.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* sockets */
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define CHANCE 6
+#define MAX 0x100
+#define CANTIDAD(x) (sizeof((x))/sizeof(char*)-1)
+
+const char *palabra[]={"ABEJA","AGUILA","BALLENA","BESUGO","BUHO","BUITRE","BURRO"
+	"CABALLO","CAMALEON","CAMELLO","CANGURO","CANARIO","CANGREJO","CARACOL",
+	"CASTOR","CEBRA","CERDO","CHACAL","CHIMPANCE","CHINCHILLA","CIEMPIES",
+	"CIERVO","CISNE","COBAYA","COCODRILO","COLIBRI","CHANGO","COMADREJA","CONEJO",
+	"CORMORAN","CONDOR","COTORRA","COYOTE","DELFIN","DRAGON","ESCORPION",
+	"FLAMENCO","FOCA","GALLINA","GANSO","GATO","GAVILAN","HALCON","HAMSTER",
+	"HIENA","HIPOPOTAMO","HORMIGA","HURON","IGUANA","IMPALA","JABALI","JIRAFA",
+	"JAGUAR","KIWI","KOALA","LAGARTO","LANGOSTA","LECHUZA","LEMUR","LEON",
+	"LEOPARDO","LIBELULA","LIEBRE","LINCE","OSOPARDO"};
+
+char inttochar(int x) {
+	return x+'0';
+}
+
+int main() {
+
+	struct sockaddr_in servidor_direccion;
+    struct sockaddr_in cliente_direccion;
+    int tam_struct_servidor;
+    int tam_struct_cliente;
+    int puerto;
+
+	int socket_servidor, socket_cliente;
+	int socket_bind;
+	int socket_listen;
+	char cadenaCliente[1024];
+    char cadenaServidor[1024];
+
+	socket_servidor = socket(AF_INET, SOCK_STREAM, 0); // creacion del socket
+	if(socket_servidor == -1) {
+        printf("No se puede crear el socket \n");
+        exit(-1);
+    }
+    printf("Ingrese el puerto de escucha:\n");
+    scanf("%d", &puerto);
+
+	servidor_direccion.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servidor_direccion.sin_family = AF_INET;
+    servidor_direccion.sin_port = puerto;
+    tam_struct_servidor = sizeof(servidor_direccion);
+
+	socket_bind = bind(socket_servidor, (struct sockaddr *)&servidor_direccion, tam_struct_servidor);
+	if(socket_bind == -1) {
+		printf("No se puede crear el bind\n");
+		exit(-1);
+	}
+
+	int backlog = 1;
+	socket_listen = listen(socket_servidor, backlog);
+	if(socket_listen == -1) {
+		printf("No se puede crear el listen\n");
+		exit(-1);
+	}
+	printf("Servidor en espera...\n");
+
+	while(1) {
+		socket_cliente = accept(socket_servidor, (struct sockaddr *)&cliente_direccion, &tam_struct_servidor);
+		if(socket_cliente == -1) {
+			printf("No se puede crear el accept \n");
+			exit(-1);
+		}
+		printf("\t-- CLIENTE CONECTADO --\n");
+
+		const char *elegida;
+		srand(time(NULL));
+		int aleatoria;
+		aleatoria = rand()%CANTIDAD(palabra);
+
+		elegida = palabra[aleatoria];
+		printf("%s | %ld\n", elegida, strlen(elegida));
+		int x = strlen(elegida);
+
+		int oportunidades = CHANCE;
+
+		int N = strlen(elegida);
+		int mask[N];
+		char aux[1024];
+		for (int i=0; i < N; ++i) {
+		    mask[i] = 0;
+		}
+		char copia[1024];
+		strcpy(copia, elegida);
+		int cont = 0;
+		int gameover = 0;
+		char opor[1024] = "";
+		while(strcmp(cadenaServidor, "SALIR") != 0 && gameover == 0) {
+			strcpy(opor, "NO");
+			int flag = 0;
+			send(socket_cliente, "\nLa palabra es: ", 1024, 0);
+			for(int j=0; j < N; ++j) {
+				if (mask[j] == 1) {
+				    aux[j] = copia[j];
+				}
+				else {
+				    aux[j] = '_';
+				}
+			}
+
+			send(socket_cliente, aux, 1024, 0);
+
+			char guess[2];
+			send(socket_cliente, "\nIngrese una ", 1024, 0);
+			recv(socket_cliente, guess, 1024, 0);
+
+			for(int k=0; k < N; ++k) {
+				if (copia[k] == guess[0]) {
+				    mask[k] = 1;
+				    flag = 1;
+				    strcpy(opor, "SI");
+				}
+			}
+			send(socket_cliente, opor, 1024, 0);
+			if(flag == 0) {
+				oportunidades--;
+				char c = inttochar(oportunidades);
+				char o[50] = "Te quedan ";
+				int x = strlen(o);
+				o[strlen(o)] = c;
+				strcat(o, " oportunidades.\n");
+				send(socket_cliente, o, 1024, 0);
+				if(oportunidades == 0) {
+				   	send(socket_cliente, "BREAK", 1024, 0);
+				    break;
+				}
+			}
+
+			for(int m = 0; m < N; ++m) {
+				cont += mask[m];
+				if (mask[m] == 0) {
+				    cont = 0;
+				    break;
+				} else if(cont == N) {
+				   	gameover = 1;
+				    send(socket_cliente, "BREAK", 1024, 0);
+				}
+			}
+		}
+		if(oportunidades == 0) {
+			char p[50] = "\nPerdiste! La palabra es ";
+			copia[N] = '\n';
+			copia[N+1] = '\0';
+			strcat(p, copia);
+			send(socket_cliente, p, 1024, 0);
+		} else {
+			char g[50] = "\nGanaste! La palabra es ";
+			copia[N] = '\n';
+			copia[N+1] = '\0';
+			strcat(g, copia);
+			send(socket_cliente, g, 1024, 0);
+		}
+		close(socket_cliente);
+	}
+	close(socket_servidor);
+
+}
